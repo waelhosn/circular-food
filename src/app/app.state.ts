2a757e3c50e608713016aa355085173f692a708f
waelhosn <wael__1995@hotmail.com>
2018-07-30 14:51:35 +0300

    add files

--- src/app/app.state.ts
+++
@@ -1,6 +0,0 @@
-import { Menu } from '././models/Menu.model';
-
-export interface AppState {
-  readonly menu: Menu[];
-  readonly loading: boolean;
-}
--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Menu } from '../models/menu.model';
 import { Observable } from 'rxjs/Observable';
-import { AppState } from './../app.state';
+
 import { Store } from '@ngrx/store';
 import * as fromRoot from '../actions/reducer';
 import * as Actions from '../actions/menu.action';
@@ -16,8 +16,8 @@ export class SearchResultsComponent implements OnInit {
   results: Observable<Menu[]>;
   loading: Observable<Boolean>;
 
-  constructor( private store: Store<AppState>) {
-    this.results = this.store.select(state => state.menu);
+  constructor( private store: Store<fromRoot.State>) {
+    this.results = this.store.select(state => state.results);
     this.loading = this.store.select(state => state.loading);
    }
 
